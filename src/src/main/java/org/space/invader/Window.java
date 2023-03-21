c50eb8cbf77beabf2929ec7d77823a934d9cdec8
Bingdididi <zhoubingdica@gmail.com>
2023-03-20 21:33:50 -0700

    Created the Constant class and updated the Window class

--- src/src/main/java/org/space/invader/Window.java
+++ src/src/main/java/org/space/invader/Window.java
@@ -3,6 +3,7 @@ import javax.swing.*;
 import java.awt.*;
 
 public class Window extends JPanel {
+  public InvaderManager groupInvaders = new InvaderManager();
 
   public static final int WINDOW_SIZE = 600;
   public static final int WINDOW_HEIGHT = 600;
@@ -20,15 +21,23 @@ public class Window extends JPanel {
     g2.setColor(Color.BLACK);
     g2.fillRect(0,0, WINDOW_SIZE, WINDOW_HEIGHT);
 
+    //Draw the green line on the bottom of the window
+    g2.setColor(Color.GREEN);
+    g2.fillRect(30, 530, 535, 5);
 
-
-
-
-
-
+    //Draw the invaders
+    this.groupInvaders.drawInvader(g2);
 
   }
-
+  public static void main(String[] args) {
+    JFrame frame = new JFrame("Space Invaders");
+    frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+    Window window = new Window();
+    window.setPreferredSize(new Dimension(WINDOW_SIZE, WINDOW_HEIGHT));
+    frame.add(window);
+    frame.pack();
+    frame.setVisible(true);
+  }
 
 
 
