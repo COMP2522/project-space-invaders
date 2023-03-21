668305b369f7556f34a1afe9195a724db6e85920
taehyuk <7374179@gmail.com>
2023-03-21 12:51:12 -0700

    Constant updating

--- src/src/main/java/org/space/invader/Constant.java
+++ src/src/main/java/org/space/invader/Constant.java
@@ -7,9 +7,34 @@ public class Constant {
   public static final int WINDOW_HEIGHT = 600;
   public static final int WINDOW_MARGIN = 50;
 
+  /************************************* VAISSEAU *************************************/
+  // vaisseau = player
+  // Dimensions du vaisseau
+//  public static final int LARGEUR_VAISSEAU = 39;
+  public static final int PLAYER_SIZE = 39;
+//  public static final int HAUTEUR_VAISSEAU = 24;
+  public static final int PLAYER_HEIGHT = 24;
+
+  // Position initiale du vaisseau
+//  public final static int X_POS_INIT_VAISSEAU = (LARGEUR_FENETRE - LARGEUR_VAISSEAU)/ 2;
+  public final static int X_POS_INIT_PLAYER = (WINDOW_SIZE - PLAYER_SIZE)/ 2;
+//  public final static int Y_POS_VAISSEAU = 490;
+  public final static int Y_POS_PLAYER = 490;
+
+  // Unit?de d?lacement du vaisseau
+//  public final static int DX_VAISSEAU = 1;
+  public final static int DX_PLAYER = 1;
+
+  // Limite de d?lacement du vaisseau
+//  public final static int LIMITE_GAUCHE_VAISSEAU = 60;
+  public final static int LIMIT_LEFT_PLAYER = 60;
+//  public final static int LIMITE_DROITE_VAISSEAU = 500;
+  public final static int LIMIT_RIGHT_VAISSEAU = 500;
 
 
 
+
+  /************************************* ALIEN ***************************************/
 
   //Invader size
   public static final int INVADER_SIZE = 33;
