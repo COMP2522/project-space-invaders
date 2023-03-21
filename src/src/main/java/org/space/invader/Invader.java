51f69a49462fdb88b6ee63bb94978a1f4fa5903b
Bingdididi <zhoubingdica@gmail.com>
2023-03-20 21:41:34 -0700

    Completed the Invader class and InvaderManager class

--- src/src/main/java/org/space/invader/Invader.java
+++ src/src/main/java/org/space/invader/Invader.java
@@ -1,15 +1,41 @@
 package org.space.invader;
 
-public class Invader {
+public class Invader extends Sprite {
 
   //Alien size
-  public static final int alienSize = 33;
-  public static final int alienHeight = 25;
-
-  //
+  public static final int INVADER_SIZE = 33;
+  public static final int INVADER_HEIGHT = 25;
 
 
+  //Alien position
+  public final static int ALT_INIT_INVADER = 120;
+  public final static int XPOS_INIT_INVADER = 29 + Window.WINDOW_MARGIN;
+  public final static int SPACE_ROW_INVADER = 40;
+  public final static int SPACE_COLOMN_INVADER = 10;
 
+  //Alien movement unit
+
+  public final static int DX_INVADER= 5;
+  public final static int DY_INVADER= 25;
+
+
+public Invader(int xPos, int yPos, String strImg1, String strImg2 ){
+  //Initialize the variables of the super class
+  super.xPos = xPos;
+  super.yPos = yPos;
+  super.size = INVADER_SIZE;
+  super.height = INVADER_HEIGHT;
+  super.dx = 0;
+  super.dy = 0;
+  //Address of the images
+  super.strImg1 = strImg1;
+  super.strImg2 = strImg2;
+//  super.strImg3 = strImg3;
+
+
+
+
+}
 
 
 }
--- src/src/main/java/org/space/invader/InvaderManager.java
+++ src/src/main/java/org/space/invader/InvaderManager.java
@@ -1,4 +1,43 @@
 package org.space.invader;
 
+import java.awt.Graphics;
+
+
 public class InvaderManager {
+
+  private Invader tabInvader[][] = new Invader[5][10];
+
+  public InvaderManager(){
+    this.initTableInvaders();
+  }
+
+private void initTableInvaders(){
+    //create the invader table
+  for(int column = 0; column < 10; column++){
+    this.tabInvader[0][column] = new Invader(Constant.XPOS_INIT_INVADER + (Constant.INVADER_SIZE + Constant.SPACE_COLOMN_INVADER)* column,
+            Constant.ALT_INIT_INVADER, "/image/alien1.svg", "/image/alien1.svg");
+    for(int row = 1; row <3; row++){
+      this.tabInvader[row][column] = new Invader(Constant.XPOS_INIT_INVADER + (Constant.INVADER_SIZE + Constant.SPACE_COLOMN_INVADER)* column,
+              Constant.ALT_INIT_INVADER + Constant.SPACE_ROW_INVADER * row, "/image/alien2.svg", "/image/alien2.svg"  );
+    }
+    for(int row = 3; row <5; row++){
+      this.tabInvader[row][column] = new Invader(Constant.XPOS_INIT_INVADER + (Constant.INVADER_SIZE + Constant.SPACE_COLOMN_INVADER)* column,
+              Constant.ALT_INIT_INVADER + Constant.SPACE_ROW_INVADER * row, "/image/alien3.svg", "/image/alien3.svg"  );
+    }
+
+  }
+}
+public void drawInvader(Graphics g){
+    //Draw the invaders in the invader table
+  for(int column = 0; column < 10; column++){
+    for(int row = 0; row < 5; row++){
+      g.drawImage(this.tabInvader[row][column].getImg(), this.tabInvader[row][column].getxPos(),
+              this.tabInvader[row][column].getyPos(), null );
+    }
+  }
+}
+
+
+
+
 }
